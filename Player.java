a934d8bb2e0c04e563bebcffc720e1fc88f37945
darkstar8500 <darkstar8500@users.noreply.github.com>
2016-05-16 09:34:48 -0700

    Add files via upload

--- Player.java
+++ Player.java
@@ -5,17 +5,18 @@ public class Player
     private int myLives;
     private Location myLoc;
     private Platform myPlatform;
+    private Polygon myPoly;
     
     public Player(Platform p)
     {
         myLives = 5;
         myLoc = new Location();
         myPlatform = p;
-    }
-    
-    public int getLives()
-    {
-        return myLives;
+        myPoly = new Polygon();
+        myPoly.addPoint(myLoc.getX(), myLoc.getY());
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY());
+        myPoly.addPoint(myLoc.getX(), myLoc.getY() + 2);
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY() + 2);
     }
     
     public Location getLocation()
@@ -31,6 +32,11 @@ public class Player
     public void setLocation(Location l)
     {
         myLoc = l;
+        myPoly = new Polygon();
+        myPoly.addPoint(myLoc.getX(), myLoc.getY());
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY());
+        myPoly.addPoint(myLoc.getX(), myLoc.getY() + 2);
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY() + 2);
     }
     
     public void setPlatform(Platform p)
@@ -41,11 +47,21 @@ public class Player
     public void jump()
     {
         myLoc.changeY(10);
+        myPoly = new Polygon();
+        myPoly.addPoint(myLoc.getX(), myLoc.getY() + 10);
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY() + 10);
+        myPoly.addPoint(myLoc.getX(), myLoc.getY() + 12);
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY() + 12);
     }
     
     public void fall()
     {
         myLoc.changeY(-5);
+        myPoly = new Polygon();
+        myPoly.addPoint(myLoc.getX(), myLoc.getY() - 5);
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY() - 5);
+        myPoly.addPoint(myLoc.getX(), myLoc.getY() - 3);
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY() - 3);
     }
     
     public void moveLeft()
@@ -53,16 +69,31 @@ public class Player
         if (myPlatform.getOrientation() == 1)
         {
             myLoc.changeX(-10);
+            myPoly = new Polygon();
+            myPoly.addPoint(myLoc.getX() - 10, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() - 8, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() - 10, myLoc.getY() + 2);
+            myPoly.addPoint(myLoc.getX() - 8, myLoc.getY() + 2);
         }
         else if (myPlatform.getOrientation() == 0)
         {
             myLoc.changeX(-10);
             myLoc.changeY((int)(10 / Math.sqrt(3)));
+            myPoly = new Polygon();
+            myPoly.addPoint(myLoc.getX() - 10, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() - 8, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() - 10, myLoc.getY() + 2);
+            myPoly.addPoint(myLoc.getX() - 8, myLoc.getY() + 2);
         }
         else
         {
             myLoc.changeX(-10);
             myLoc.changeY((int)(-10 / Math.sqrt(3)));
+            myPoly = new Polygon();
+            myPoly.addPoint(myLoc.getX() - 10, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() - 8, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() - 10, myLoc.getY() + 2);
+            myPoly.addPoint(myLoc.getX() - 8, myLoc.getY() + 2);
         }
     }
     
@@ -71,21 +102,46 @@ public class Player
         if (myPlatform.getOrientation() == 1)
         {
             myLoc.changeX(10);
+            myPoly = new Polygon();
+            myPoly.addPoint(myLoc.getX() + 10, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() + 12, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() + 10, myLoc.getY() + 2);
+            myPoly.addPoint(myLoc.getX() + 12, myLoc.getY() + 2);
         }
         else if (myPlatform.getOrientation() == 0)
         {
             myLoc.changeX(10);
             myLoc.changeY((int)(-10 / Math.sqrt(3)));
+            myPoly = new Polygon();
+            myPoly.addPoint(myLoc.getX() + 10, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() + 12, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() + 10, myLoc.getY() + 2);
+            myPoly.addPoint(myLoc.getX() + 12, myLoc.getY() + 2);
         }
         else
         {
             myLoc.changeX(10);
             myLoc.changeY((int)(10 / Math.sqrt(3)));
+            myPoly = new Polygon();
+            myPoly.addPoint(myLoc.getX() + 10, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() + 12, myLoc.getY());
+            myPoly.addPoint(myLoc.getX() + 10, myLoc.getY() + 2);
+            myPoly.addPoint(myLoc.getX() + 12, myLoc.getY() + 2);
         }
     }
     
     public void climb()
     {
         myLoc.changeY(5);
+        myPoly = new Polygon();
+        myPoly.addPoint(myLoc.getX(), myLoc.getY() + 5);
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY() + 5);
+        myPoly.addPoint(myLoc.getX(), myLoc.getY() + 7);
+        myPoly.addPoint(myLoc.getX() + 2, myLoc.getY() + 7);
+    }
+    
+    public void loseLife()
+    {
+        myLives -= 1;
     }
 }
