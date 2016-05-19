f6bf489da10303f099bc90279ae4580265a24d9f
mdust9 <matthew.r.duston@gmail.com>
2016-05-19 10:19:55 -0700

    Update InstructionsState.java

--- InstructionsState.java
+++ InstructionsState.java
@@ -3,51 +3,45 @@ package GameState;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 
-import javax.imageio.ImageIO;
-
-import Keys.Keys;
 import GUI.GamePanel;
+import Keys.Keys;
 
-public class InstructionsState extends GameState {
-	private Font font;
+public class CreditsState extends GameState{
+		private Font font;
 
-	public InstructionsState(GameStateManager gsm) {
+		public CreditsState(GameStateManager gsm) {
 
-		super(gsm);
-try {
-	font = new Font("Century Gothic", Font.PLAIN, 14);
+			super(gsm);
+	try {
+		font = new Font("Century Gothic", Font.PLAIN, 14);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
-		catch(Exception e) {
-			e.printStackTrace();
+
+		public void init() {}
+
+		public void update() {
+			handleInput();
 		}
-	}
 
-	public void init() {}
+		public void draw(Graphics2D g) {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+			
+			g.setColor(Color.WHITE);
+			g.setFont(font);
+			g.drawString("CREDITS", 35, 30);
+			g.drawString("Martin Shkreli", 25, 70);
+			g.drawString("Anjuman Raha", 25, 90);
+			g.drawString("Anish Sathe", 25, 110);
+			g.drawString("Chaitanya Kore", 25, 130);
+		}
 
-	public void update() {
-		handleInput();
-	}
-
-	public void draw(Graphics2D g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
-		
-		g.setColor(Color.WHITE);
-		g.setFont(font);
-		g.drawString("INSTRUCTIONS", 35, 30);
-		g.drawString("Try to reach the goal without hitting the", 25, 70);
-		g.drawString("   obstacles", 25, 90);
-		g.drawString("Press the Left and Right arrow keys to move", 25, 110);
-		g.drawString("   left or right respectively", 25, 130);
-		g.drawString("Press the space button to jump, up arrow", 25, 150);
-		g.drawString("   button to move up a ladder", 25, 170);
-		g.drawString("If you run into an obstacle, you lose", 25, 190);
-	}
-
-	public void handleInput() {
-		if (Keys.isPressed(Keys.ESCAPE))
-			gsm.setState(GameStateManager.MENUSTATE);
-	}
+		public void handleInput() {
+			if (Keys.isPressed(Keys.ESCAPE))
+				gsm.setState(GameStateManager.MENUSTATE);
+		}
 }
